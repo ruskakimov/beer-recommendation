65551a6c5728c0f34681a66ad9f35a2bd0d117b3
Rustem Kakimov <ruskakimov@gmail.com>
2024-03-03 21:29:21 -0500

    Recommendations table

--- src/tabs/Recommendations.tsx
+++ src/tabs/Recommendations.tsx
@@ -1,3 +1,50 @@
+import {
+  Paper,
+  Rating,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+const rows: [string, number][] = [
+  ["Sausa Pils", 5],
+  ["Blue Moon", 4.5],
+  ["Hoegaarden", 4],
+  ["Sausa Wels", 3.5],
+  ["Budweiser", 3.5],
+];
+
 export default function Recommendations() {
-  return <div>Recommendations</div>;
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table aria-label="rating table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Beer</TableCell>
+              <TableCell align="right">Rating</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(([name, rating]) => (
+              <TableRow
+                key={name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {name}
+                </TableCell>
+                <TableCell align="right">
+                  <Rating name={name} value={rating} readOnly precision={0.5} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
